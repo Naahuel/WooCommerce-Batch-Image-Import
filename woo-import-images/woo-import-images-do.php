3f0b554832dadc6a5beaae028ac4cafe0ffb9f56
Nahuel José <nahuel@nahueljose.com.ar>
2016-08-24 11:42:07 -0300

    Translate to English; Tidy up.

--- woo-import-images/woo-import-images-do.php
+++ woo-import-images/woo-import-images-do.php
@@ -1,74 +1,93 @@
-<h1>Importar Imágenes</h1>
-
+<h1>WooCommerce Import Images</h1>
+<p>
+	Make sure you read how it works and the warnings here: <a target="_blank" href="https://github.com/Naahuel/WooCommerce-Batch-Image-Import">https://github.com/Naahuel/WooCommerce-Batch-Image-Import</a>
+</p>
+<p>
+	<a href="<?php menu_page_url('woo-import-images/woo-import-images-do.php'); ?>&amp;do-it=go" class="button button button-primary">Run</a>
+</p>
 <?php
 
-// Get the path to the upload directory.
-$wp_upload_dir = wp_upload_dir();
+if( isset($_GET['do-it']) ){
 
-// Directorio de productos
-$products_dir = $wp_upload_dir['basedir'] . '/products';
+	// Get the path to the upload directory.
+	$wp_upload_dir = wp_upload_dir();
 
-$files = array_diff(scandir($products_dir), array('.', '..'));
+	// Get the path to the products dir, where images are
+	$products_dir = $wp_upload_dir['basedir'] . '/products';
 
-// Need to require these files
-if ( !function_exists('media_handle_upload') ) {
-	require_once(ABSPATH . "wp-admin" . '/includes/image.php');
-	require_once(ABSPATH . "wp-admin" . '/includes/file.php');
-	require_once(ABSPATH . "wp-admin" . '/includes/media.php');
-}
+	// Get all files, alphabetically
+	$files = array_diff(scandir($products_dir), array('.', '..'));
 
-foreach ($files as $_file) {
-
-	$file_array = array();
-	$filename = $products_dir . '/' . $_file;
-	$file_info = pathinfo($filename);
-	$sku = explode('-',$file_info['filename']);
-
-	// Set variables for storage
-	// fix file filename for query strings
-	preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $filename, $matches);
-	$file_array['name'] = basename($matches[0]);
-	$file_array['tmp_name'] = $filename;
-
-	echo '<h3>' .  $sku[0] . '</h3>' . '<br>';
-
-	$post_id = wc_get_product_id_by_sku( $sku[0] );
-
-	if( $post_id ){
-		$the_post = get_post( $post_id );
-
-		$description = $the_post->post_title;
-		echo $description . '<br>';
-
-		// Subir la imagen
-		$id = media_handle_sideload( $file_array, $post_id, $description );
-
-		// Adjuntar al post
-		if( has_post_thumbnail($post_id) ){
-			// El post ya tiene imagen
-			echo  'El post ya tiene imagen' . '<br>';
-
-			// Actualizar galería de WooCommerce
-			$galeria = get_post_meta( $post_id, '_product_image_gallery', true );
-			$ids = $galeria ?  $galeria . ',' . $id : $id;
-			update_post_meta( $post_id, '_product_image_gallery', $ids);
-
-		} else {
-			// NO tiene imagen
-			echo  'Imagen nueva' . '<br>';
-
-			// Establecer como imagen destacada
-			set_post_thumbnail( $post_id, $id );
-		}
-
-		// Borrar archivo
-		@unlink($file_array['tmp_name']);
-	} else {
-
-		// EL post / producto no existe
-		echo '<span style="color:red;">No existe el producto</span>';
+	// Need to require these files? Probably not.
+	if ( !function_exists('media_handle_upload') ) {
+		require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+		require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+		require_once(ABSPATH . "wp-admin" . '/includes/media.php');
 	}
 
-	echo '<hr>';
+	// Loop through the files
+	foreach ($files as $_file) {
 
-} // endforeach
+		// Set up file information
+		$file_array = array();
+		$filename 	= $products_dir . '/' . $_file;
+		$file_info	= pathinfo($filename);
+		$sku 				= explode('-',$file_info['filename']);
+
+		// Fix file filename for query strings
+		preg_match('/[^\?]+\.(jpg|jpe|jpeg|gif|png)/i', $filename, $matches);
+		$file_array['name'] 		= basename($matches[0]);
+		$file_array['tmp_name'] = $filename;
+
+		// Show SKU
+		echo '<h3>' .  $sku[0] . '</h3>';
+
+		// Get product id, by its SKU
+		$post_id = wc_get_product_id_by_sku( $sku[0] );
+
+		// Check if product exists
+		if( $post_id ){
+
+			// It exists. Get the product.
+			$the_post = get_post( $post_id );
+
+			// Image description
+			$description = $the_post->post_title;
+			echo '<strong>' . $description . '</strong><br>';
+
+			// Upload image to the library
+			$id = media_handle_sideload( $file_array, $post_id, $description );
+
+			// Attach to post
+			if( has_post_thumbnail($post_id) ){
+
+				// The post already has a thumbnail.
+				echo  'The post already has a thumbnail. Adding to product gallery.' . '<br>';
+
+				// Update product gallery
+				$gallery = get_post_meta( $post_id, '_product_image_gallery', true );
+				$ids = $gallery ?  $gallery . ',' . $id : $id;
+				update_post_meta( $post_id, '_product_image_gallery', $ids);
+
+			} else {
+
+				// No thumbnail
+				echo  'Setting post thumbnail.' . '<br>';
+
+				// Set post thumbnail
+				set_post_thumbnail( $post_id, $id );
+			}
+
+			// Delete file
+			@unlink($file_array['tmp_name']);
+		} else {
+
+			// post/product doesn't exist.
+			echo '<span style="color:red;">Product does not exist</span>';
+		}
+
+		echo '<hr>';
+
+	} // endforeach
+
+} //isset($_GET['do-it'])
